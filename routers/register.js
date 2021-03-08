8590a6fc19a1debf4c215159369df131c22c22f4
Grga0701 <55957842+Grga0701@users.noreply.github.com>
2021-03-08 13:04:15 +0100

    Update register.js

--- routers/register.js
+++ routers/register.js
@@ -5,8 +5,8 @@ const registerService = require('../services/register');
 
 router.post('/api/register', async (req, res)=>{
     try {
-        registerService.register(req);
-        res.send('user has been saved')
+        var msg = await registerService.register(req);
+        res.send(msg)
     } catch (error) {
         res.status(500).send(error)
     }
