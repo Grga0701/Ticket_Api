2201ba0aa2408d75bdf196a31d03dd94755cc533
Grga0701 <55957842+Grga0701@users.noreply.github.com>
2021-03-08 14:51:00 +0100

    Add files via upload

---
+++ services/register.js
@@ -0,0 +1,41 @@
+const User = require('../models/user');
+const bcrypt =  require('bcrypt');
+
+
+async function register(req) {
+    if(req.body.password == "" || req.body.user_name == ""){
+        var msg ="You didnt enter username or password";
+        return msg;
+    }
+    const username_check = await User.findOne({ user_name: req.body.user_name}, function(err, user){
+        if(err){
+            console.log(err)
+            return res.send("error");
+        }
+    })
+    if (username_check != null){
+        msg ="user allready exists";
+        return msg;
+    }
+    const salt = await bcrypt.genSalt();
+    const hashdPassword = await bcrypt.hash(req.body.password, salt)
+    const user = new User({
+        user_name: req.body.user_name,
+        password: hashdPassword,
+        email: req.body.email,
+        status: req.body.status,
+        credit_card: req.body.credit_card
+    })
+    user.save()
+        .then((result)=>{
+            return result;
+        })
+        .catch((err)=>{
+            console.log(err)
+        })
+    msg ="You have been registerd";
+    return msg;
+
+};
+
+module.exports = { register }
